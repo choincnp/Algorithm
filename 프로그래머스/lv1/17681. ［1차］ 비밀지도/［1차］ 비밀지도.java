6dd37d4866096b227c67dcff914841b350ce65ba
Cho Youngjun <108874833+choincnp@users.noreply.github.com>
2023-01-26 08:45:14 +0900

    [level 1] Title: [1차] 비밀지도, Time: 6.31 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,15 @@
+class Solution {
+  public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] result = new String[n];
+        for (int i = 0; i < n; i++) {
+            result[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            result[i] = String.format("%" + n + "s", result[i]);
+            result[i] = result[i].replaceAll("1", "#");
+            result[i] = result[i].replaceAll("0", " ");
+        }
+        return result;
+    }
+}
