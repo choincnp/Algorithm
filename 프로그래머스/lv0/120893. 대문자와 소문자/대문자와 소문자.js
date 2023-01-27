b6ee5283721bd40f4b0cbad3aa89f0335f8d4f4c
Cho Youngjun <108874833+choincnp@users.noreply.github.com>
2023-01-28 02:34:48 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.30 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    let answer = ""
+    for(let i=0; i<my_string.length; i++){
+        if(my_string[i] === my_string[i].toUpperCase()){
+            answer += my_string[i].toLowerCase()
+        }else answer += my_string[i].toUpperCase()
+    }
+    return answer
+}
