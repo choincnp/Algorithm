af579dc14c99c60bdf9bec8d5d10c9287bbb6a53
Cho Youngjun <108874833+choincnp@users.noreply.github.com>
2023-01-27 09:37:44 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.java
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -1,10 +1,5 @@
 class Solution {
     public String solution(int num) {
-        String answer = "";
-        if (num%2==0)
-            answer += "Even";
-        else
-            answer += "Odd";
-        return answer;
+        return (num%2==0)? "Even" : "Odd";
     }
 }
