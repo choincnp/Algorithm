ae82a74889e8262e79a3f8d4ced5cfbe816044aa
Cho Youngjun <108874833+choincnp@users.noreply.github.com>
2023-01-30 20:24:34 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.java
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -3,35 +3,15 @@ import java.util.Iterator;
 class Solution {
     public String solution(int a, int b) {
         String answer = "";
-        Map<Integer, Integer> month = new TreeMap<>();
-        Map<Integer, String> day = new TreeMap<>();
-        month.put(0,31);
-        month.put(1,29);
-        month.put(2,31);
-        month.put(3,30);
-        month.put(4,31);
-        month.put(5,30);
-        month.put(6,31);
-        month.put(7,31);
-        month.put(8,30);
-        month.put(9,31);
-        month.put(10,30);
-        day.put(1,"FRI");
-        day.put(2,"SAT");
-        day.put(3,"SUN");
-        day.put(4,"MON");
-        day.put(5,"TUE");
-        day.put(6,"WED");
-        day.put(0,"THU");
-        Iterator<Integer> it = month.keySet().iterator();
-        a -= 1;
-        int i=0;
-        while (it.hasNext()){
-            if (it.next() < a) b += month.get(i);
-            else break;
-            i++;
+        int[] month = {31,29,31,30,31,30,31,31,30,31,30,31};
+        String[] date = {"FRI","SAT","SUN","MON","TUE","WED","THU"};
+        int total = 0;
+        
+        for (int i=0; i<a-1; i++){
+            total += month[i];
         }
-        answer = day.get(b%7);
+        total = total + b-1;
+        answer = date[total%7];
         return answer;
     }
 }
