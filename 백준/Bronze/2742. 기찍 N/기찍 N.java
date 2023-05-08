9152867745700220c0a4d47c75a0be830e75665e
Cho Youngjun <108874833+choincnp@users.noreply.github.com>
2023-05-09 04:06:31 +0900

    [Bronze IV] Title: 기찍 N, Time: 1544 ms, Memory: 33208 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for (int i=0; i<N; i++){
+			System.out.println(N-i);
+		}
+	}
+}
