b8f1c05353364949fc13f61a36ff3929c9fe84cb
Cho Youngjun <108874833+choincnp@users.noreply.github.com>
2023-05-25 03:36:38 +0900

    [unrated] Title: 전국 대회 선발 고사, Time: 5.80 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -2,27 +2,12 @@ import java.util.stream.*;
 import java.util.*;
 class Solution {
     public int solution(int[] rank, boolean[] attendance) {
-        int[] value = new int[]{10000,100,1};
-		int[][] rankScore = new int[rank.length][2];
-		for (int i=0; i<rank.length; i++){
-			rankScore[i][0] = rank[i];
-			rankScore[i][1] = i;
-		}
-		Arrays.sort(rankScore, new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o1[0]-o2[0];
-			}
-		});
-		int sum = 0;
-		int count = 0;
-		for (int i=0; i<rankScore.length; i++){
-			if (count == 3) break;
-			if (attendance[rankScore[i][1]]){
-				sum += value[count]*rankScore[i][1];
-				count++;
-			}
-		}
-		return sum;
+        return IntStream.range(0, rank.length)
+			.filter(i -> attendance[i])
+			.boxed()
+			.sorted(Comparator.comparing(i -> rank[i]))
+			.limit(3)
+			.reduce((current, next) -> current*100 + next)
+			.get();
     }
 }
