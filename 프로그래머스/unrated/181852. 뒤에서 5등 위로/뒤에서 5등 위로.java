02397fc6ca1e0290f7b71a4d896c8ae439beaa98
Cho Youngjun <108874833+choincnp@users.noreply.github.com>
2023-05-25 02:33:29 +0900

    [unrated] Title: 뒤에서 5등 위로, Time: 3.82 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -1,7 +1,7 @@
 import java.util.*;
+import java.util.stream.*;
 class Solution {
     public int[] solution(int[] num_list) {
-        Arrays.sort(num_list);
-        return Arrays.copyOfRange(num_list,5, num_list.length);
+        return Arrays.stream(num_list).sorted().skip(5).toArray();
     }
 }
