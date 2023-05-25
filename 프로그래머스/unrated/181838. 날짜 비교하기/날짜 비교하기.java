4b3c9267a87eda69debcbeb08d3db26c887ab0c9
Cho Youngjun <108874833+choincnp@users.noreply.github.com>
2023-05-26 03:05:27 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.03 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int[] date1, int[] date2) {
+        if (date1[0] < date2[0]) return 1;
+		else if (date1[0]==date2[0] && date1[1] < date2[1]) return 1;
+		else if (date1[0]==date2[0] && date1[1]==date2[1] && date1[2] < date2[2]) return 1;
+		else return 0;
+    }
+}
