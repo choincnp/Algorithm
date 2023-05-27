2d516e6087e52a1ad53c767c3b7f56696390b449
Cho Youngjun <108874833+choincnp@users.noreply.github.com>
2023-05-27 21:19:02 +0900

    [unrated] Title: 특별한 이차원 배열 2, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
+++ 프로그래머스/unrated/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
@@ -1,17 +1,10 @@
 class Solution {
     public int solution(int[][] arr) {
-        int[][] arr2 = new int[arr.length][arr.length];
         for (int i=0; i<arr.length; i++){
-            for (int j=0; j<arr.length; j++){
-                arr2[j][i] = arr[i][j];
+            for (int j=0; j<i; j++){
+                if (arr[i][j] != arr[j][i]) return 0;
             }
         }
-        boolean check = true;
-        for (int i=0; i<arr.length; i++){
-            for (int j=0; j<arr.length; j++){
-                if (arr[i][j] != arr[j][i]) check = false;
-            }
-        }
-        return check? 1 : 0;
+        return 1;
     }
 }
