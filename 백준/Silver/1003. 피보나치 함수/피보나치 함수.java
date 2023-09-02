43384d27e65579cadb52ef2405fcbceeef73973b
Cho Youngjun <108874833+choincnp@users.noreply.github.com>
2023-09-03 03:16:46 +0900

    [Silver III] Title: 피보나치 함수, Time: 84 ms, Memory: 11472 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 public class Main {
     static int[][] dp = new int[41][2];
@@ -10,7 +9,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        IntStream.rangeClosed(0, 40).forEach(i -> Arrays.fill(dp[i], -1));
+        for (int i=0; i<41; i++){
+            Arrays.fill(dp[i],-1);
+        }
         dp[0][1] = 0;
         dp[0][0] = 1;
         dp[1][1] = 1;
