812a9a03c92912a416ae9df0f5d5d4e4c489576c
Cho Youngjun <108874833+choincnp@users.noreply.github.com>
2023-09-07 01:05:27 +0900

    [Silver III] Title: 계단 오르기, Time: 76 ms, Memory: 11588 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -4,31 +4,27 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    static Integer[] dp;
+    static int[] dp;
     static int[] stairs;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         stairs = new int[T+1];
-        dp = new Integer[T+1];
+        dp = new int[T+1];
         for (int i=1; i<=T; i++){
             int score = Integer.parseInt(br.readLine());
             stairs[i] = score;
         }
-        dp[0] = stairs[0];
         dp[1] = stairs[1];
-        if (T >= 2){
-            dp[2] = stairs[1] + stairs[2];
+        if (T >=2 ){
+            dp[2] = stairs[1] +stairs[2];
         }
-        System.out.println(find(T));
-    }
-    static int find(int N){
-        if (dp[N] == null){
-            dp[N] = Math.max(find(N-2), find(N-3)+stairs[N-1]) + stairs[N];
-            // 5층의 값을 찍으려면 3층 find과 2층 find + 4층값을 찍는다
+        for (int i=3; i<=T; i++){
+            dp[i] = Math.max(dp[i-2], dp[i-3]+stairs[i-1]) + stairs[i];
         }
-        return dp[N];
+        System.out.println(dp[T]);
+
     }
 
 }
