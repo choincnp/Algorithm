de6acb1385450723aca7bad76085f3546e7d6338
Cho Youngjun <108874833+choincnp@users.noreply.github.com>
2023-09-26 18:59:11 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 716 ms, Memory: 125432 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -1,35 +1,48 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
-	static StringTokenizer st;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		String s = br.readLine();
-		int N = s.length();
-		int[][] sum = new int[26][N];
-		for (int i=0; i<s.length(); i++){
-			int ch = s.charAt(i)-'a';
-			// 6번째에 j
-			for (int j=i; j<N; j++){
-				sum[ch][j]++;
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String s = br.readLine(); //문자열
+		int q = Integer.parseInt(br.readLine()); //질문 수
+		int l=s.length();
+		
+		//누적합 기록을 위한 배열 선언
+		int[][] memos = new int[l+1][26];
+		//처음 값 횟수 증가
+		memos[1][s.charAt(0)-'a']++;
+		//나머지 문자 탐색
+		for(int i=2;i<=l;i++) {
+			//탐색 중인 문자
+			int idx = s.charAt(i-1)-'a';
+			//알파벳(a-z)탐색
+			for(int j=0;j<26;j++) {
+				//알파벳의 이전 누적 합
+				int before = memos[i-1][j];
+				//알파벳과 탐색 중인 문자가 같으면 이전값+1, 아니면 이전값과 같음
+				memos[i][j]=j==idx?before+1:before;
 			}
 		}
-		int q = Integer.parseInt(br.readLine());
-		while (q -- > 0){
-			st = new StringTokenizer(br.readLine());
-			int alpha = st.nextToken().charAt(0) - 'a';
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			if (start == 0){
-				sb.append(sum[alpha][end]).append("\n");
-			} else {
-				sb.append(sum[alpha][end] - sum[alpha][start-1]).append("\n");
-			}
+		//문제 읽기
+		StringTokenizer st;
+		for(int i=0;i<q;i++) {
+			st=new StringTokenizer(br.readLine());
+			//문제 문자
+			int idx=st.nextToken().charAt(0)-'a';
+			//시작점
+			int start=Integer.parseInt(st.nextToken())+1;
+			//끝점
+			int end=Integer.parseInt(st.nextToken())+1;
+			//끝점의 누적합 - 시작점 바로 이전의 누적합 = 구간의 문자 반복값
+			bw.append((memos[end][idx]-memos[start-1][idx])+"\n");
 		}
-		System.out.println(sb.toString());
+		bw.close();
+		br.close();
 	}
 }
