bfaa8c162115adfa9e848a2bb011dbce539aae46
Cho Youngjun <108874833+choincnp@users.noreply.github.com>
2023-09-30 17:35:42 +0900

    [Silver IV] Title: 순서쌍의 곱의 합, Time: 212 ms, Memory: 27372 KB -BaekjoonHub

---
+++ 백준/Silver/13900. 순서쌍의 곱의 합/순서쌍의 곱의 합.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		long[] arr = new long[N];
+		long[] sum = new long[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i=0; i<N; i++){
+			arr[i] = Integer.parseInt(st.nextToken());
+			if (i==0) sum[i] = arr[i];
+			else sum[i] = sum[i-1] + arr[i];
+		}
+		long answer = 0;
+		for (int i=1; i<N; i++){
+			answer += arr[i] * sum[i-1];
+		}
+		System.out.println(answer);
+	}
+}
