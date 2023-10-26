74f6ea40f80995b2c1e0f3be81e45095f0c22ed4
Cho Youngjun <108874833+choincnp@users.noreply.github.com>
2023-10-27 00:38:38 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 124 ms, Memory: 13716 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		while (sc.hasNext()){
+			System.out.println(sc.nextLine());
+		}
+	}
+}
