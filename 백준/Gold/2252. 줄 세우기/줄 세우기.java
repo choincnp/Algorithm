bd23f0d6cad01a521e733274978068c409b2a601
Cho Youngjun <108874833+choincnp@users.noreply.github.com>
2023-11-09 01:54:27 +0900

    [Gold III] Title: 줄 세우기, Time: 464 ms, Memory: 46672 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -5,35 +5,39 @@ import java.util.*;
 
 public class Main{
     static int N, M;
+    static ArrayList<ArrayList<Integer>> A;
     static StringTokenizer st;
-
-    public static void main(String[] args) throws IOException{
+    static StringBuilder sb = new StringBuilder();
+    static int[] indegree;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+        A = new ArrayList<>();
+        indegree = new int[N+1];
         for (int i=0; i<=N; i++){
-            arr.add(new ArrayList<>());
+            A.add(new ArrayList<>());
         }
-        int[] indegree = new int[N+1];
         for (int i=0; i<M; i++){
             st = new StringTokenizer(br.readLine());
             int S = Integer.parseInt(st.nextToken());
             int E = Integer.parseInt(st.nextToken());
-            arr.get(S).add(E);
+            A.get(S).add(E);
             indegree[E]++;
         }
-        Queue<Integer> queue = new LinkedList<>();
+        topology();
+        System.out.println(sb);
+    }
+    static void topology(){
+        Queue<Integer> queue = new LinkedList();
         for (int i=1; i<=N; i++){
-            if (indegree[i] == 0){
-                queue.offer(i);
-            }
+            if (indegree[i] == 0) queue.offer(i);
         }
         while (!queue.isEmpty()){
             int now = queue.poll();
-            System.out.print(now + " ");
-            for (int next : arr.get(now)){
+            sb.append(now).append(" ");
+            for (int next : A.get(now)){
                 indegree[next]--;
                 if (indegree[next] == 0){
                     queue.offer(next);
