328e34746ab0ef352463550d76b1f4fcb01ec67f
Cho Youngjun <108874833+choincnp@users.noreply.github.com>
2023-11-14 23:00:35 +0900

    [Gold IV] Title: 키 순서, Time: 580 ms, Memory: 37512 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.java
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -1,48 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main{
     static StringTokenizer st;
-    static int N, M;
+    static int N, M, taller[], shorter[];
+    static ArrayList<Integer>[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        int[][] key = new int[N+1][N+1];
+        arr = new ArrayList[N+1];
+        shorter = new int[N+1];
+        taller = new int[N+1];
         for (int i=1; i<=N; i++){
-            for (int j=1; j<=N; j++){
-                if (i==j) continue;
-                else key[i][j] = 10000001;
-            }
+            arr[i] = new ArrayList<>();
         }
         while (M-- > 0){
             st = new StringTokenizer(br.readLine());
             int S = Integer.parseInt(st.nextToken());
             int E = Integer.parseInt(st.nextToken());
-            key[S][E] = 1;
+            arr[S].add(E);
         }
-        for (int k = 1; k <= N; k++){
-            for (int i=1; i<=N; i++){
-                for (int j=1; j<=N; j++){
-                    key[i][j] = Math.min(key[i][j], key[i][k] + key[k][j]);
-                }
-            }
+        for (int i=1; i<=N; i++){
+            DFS(i, i, new boolean[N+1]);
         }
         int answer = 0;
         for (int i=1; i<=N; i++){
-            int cnt = 0;
-            for (int j=1; j<=N; j++){
-                if (key[i][j] != 10000001 || key[j][i] != 10000001){
-                    cnt++;
-                }
-            }
-            if (cnt == N) answer++;
+            if (taller[i] + shorter[i] == N-1) answer++;
         }
+
         System.out.println(answer);
+    }
+    static void DFS(int start, int now, boolean[] visited){
+        visited[now] = true;
+        for (int i : arr[now]){
+            if (!visited[i]){
+                visited[i] = true;
+                taller[start]++;
+                shorter[i]++;
+                DFS(start, i, visited);
+            }
+        }
     }
 }
