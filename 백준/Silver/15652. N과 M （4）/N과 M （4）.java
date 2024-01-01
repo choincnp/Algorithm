aaf02964e30d67b6738cbd57773a3c87fc3ad0f1
Cho Youngjun <108874833+choincnp@users.noreply.github.com>
2024-01-01 19:35:01 +0900

    [Silver III] Title: N과 M (4), Time: 132 ms, Memory: 15232 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main{
+    static int[] array;
+    static int N, M;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        array = new int[M];
+        backtracking(1, 0);
+        System.out.print(sb);
+    }
+    static void backtracking(int num, int length){
+        if (length == M){
+            for (int i : array){
+                sb.append(i).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = num; i<=N; i++){
+            array[length] = i;
+            backtracking(i, length+1);
+        }
+    }
+}
