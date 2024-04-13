73c04a58a421ab48dc538de88f52803ca346eb7a
Cho Youngjun <108874833+choincnp@users.noreply.github.com>
2024-04-13 19:02:54 +0900

    [Bronze IV] Title: R2, Time: 76 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.StringTokenizer;
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int R1 = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        System.out.println(2 * S - R1);
+    }
+}
