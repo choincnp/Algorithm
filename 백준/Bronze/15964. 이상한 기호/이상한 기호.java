166be7751bfd8f14f3ce045b9b021c760fda11ef
Cho Youngjun <108874833+choincnp@users.noreply.github.com>
2024-04-16 00:43:15 +0900

    [Bronze V] Title: 이상한 기호, Time: 80 ms, Memory: 11524 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+        System.out.println((long)Math.pow(A, 2) - (long)Math.pow(B, 2));
+    }
+}
